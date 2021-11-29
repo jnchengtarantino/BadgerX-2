332dc455754ebb6e95647d36dc1805dc8bc85397
JN <jn@chengtarantino.com>
2021-11-29 22:47:53 +0000

    test file to run steppers

--- Honey/stepperTest.py
+++ Honey/stepperTest.py
@@ -3,37 +3,28 @@ from time import sleep
 
 # GPIO mapping
 DIR = 17
-PUL = 3
-EN = 4
+PUL = 27
+EN = 22
 
 # Directions
-cw = 1
-ccw = 0
+cw = 0
+ccw = 1
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(DIR, GPIO.OUT)
 GPIO.setup(PUL, GPIO.OUT)
 GPIO.setup(EN, GPIO.OUT)
 
-GPIO.output(DIR, cw)
+GPIO.output(DIR,ccw)
 
 
 try:
 	while True:
-		sleep(1)
 		GPIO.output(DIR, cw)
 		GPIO.output(PUL, GPIO.HIGH)
 		sleep(0.005)
 		GPIO.output(PUL, GPIO.LOW)
 		sleep(0.005)
-
-		sleep(1)
-		GPIO.output(DIR, ccw)
-		GPIO.output(PUL, GPIO.HIGH)
-		sleep(0.005)
-		GPIO.output(PUL, GPIO.LOW)
-		sleep(0.005)
-
 except KeyboardInterrupt:
 	print("cleanup")
 	GPIO.cleanup()
