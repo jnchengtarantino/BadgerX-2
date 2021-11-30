b3d3b607eedeb4842cc36bebfcad74d36d4c654b
JN <jn@chengtarantino.com>
2021-11-30 01:09:05 +0000

    created example code to use controller

---
+++ Badger/controllerEx.py
@@ -0,0 +1,21 @@
+from pyPS4Controller.controller import Controller
+
+# Bluetooth controller must be paired with the pi using bluetoothctl:
+# sudo bluetoothctl
+# scan on   (to search for the MAC address)
+# pair <MAC>
+# trust <MAC>
+
+class MyController(Controller):
+   
+    def _init_(self, **kwargs):
+        Controller._init_(self, **kwargs)
+    
+    # Example of overloading button press functions
+    def on_x_press(self):
+        print("Hello World")
+    def on_x_release(self):
+        print("Goodbye World")
+
+controller = MyController(interface = "/dev/input/js0", connecting_using_ds4drv = False)
+controller.listen(timeout = 60)
