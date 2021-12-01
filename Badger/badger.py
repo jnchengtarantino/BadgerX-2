791d6f299e3ecce88bb86b03d81f78a89539104b
JN <jn@chengtarantino.com>
2021-12-01 20:00:18 +0000

    added reboot command on disconnect callback

--- Badger/badger.py
+++ Badger/badger.py
@@ -1,7 +1,11 @@
 from controller import MyController
 from adafruit_servokit import ServoKit
 from time import sleep
+import os
+
+def restart():
+	os.system('sudo reboot')
 
 pca = ServoKit(channels = 16)
 controller = MyController(pca,"/dev/input/js0", False)
-controller.listen(timeout = 60)
+controller.listen(timeout = 60, on_disconnect=restart)
