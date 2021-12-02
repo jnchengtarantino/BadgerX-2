47c9326b9f026b3e512234231c28170a319ec265
jnchengtarantino <jn@chengtarantino.com>
2021-12-02 12:57:32 -0500

    attempting to make controller read on a seperate thread

--- Badger/badger.py
+++ Badger/badger.py
@@ -1,11 +1,16 @@
 from controller import MyController
 from pcaBoard import PcaBoard
 from time import sleep
+import threading
 import os
 
 def restart():
 	os.system('sudo reboot')
+def threadFunction(controller):
+	print("Spawning controller thread")
+	controller.listen(timeout = 60) #set on_disconnect=restart for final usage
 
 pca = PcaBoard(nServos=4)
 controller = MyController(pca,"/dev/input/js0", False)
-controller.listen(timeout = 60) #set on_disconnect=restart for final usage
+controllerThread = threading.Thread(target=threadFunction, args=(controller), daemon=True)
+controllerThread.start()
