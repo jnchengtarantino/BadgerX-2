089703659c83eaf9d31d2c7cedbe142c916710b1
jnchengtarantino <jn@chengtarantino.com>
2021-12-03 14:50:30 -0500

    attempting to make motors stick controlled

--- Badger/badger.py
+++ Badger/badger.py
@@ -3,6 +3,7 @@ from pcaBoard import PcaBoard
 from time import sleep
 import threading
 import os
+import RPi.GPIO as GPIO
 
 def restart():
 	os.system('sudo reboot')
@@ -10,8 +11,10 @@ def threadFunction(controller):
 	print("Spawning controller thread")
 	controller.listen(timeout = 60) #set on_disconnect=restart for final usage
 
+GPIO.setmode(GPIO.BCM)
 pca = PcaBoard()
 pca.addServo()
+pca.addMotor(14,15)
 controller = MyController(pca,"/dev/input/js0", False)
 controllerThread = threading.Thread(target=threadFunction, args=[controller])
 controllerThread.start()
